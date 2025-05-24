e14824ae69283965f8de49f4b30c98c76e4f8949
Giovani-iDE <christian.sup08@gmail.com>
2025-05-24 17:14:25 -0500

    check port aviable

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,14 @@ use simple_logger::SimpleLogger;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  SimpleLogger::new()
-        .with_level(log::LevelFilter::Info)
-        .init()?;
+    SimpleLogger::new().with_level(log::LevelFilter::Info).init()?;
 
-    let scx = ServerContext::new().plugins_dir("plugins/").build().await;
-
+    let scx = ServerContext::new().build().await;
 
     MqttServer::new(scx)
-        .listener(
-            Builder::new()
-                .name("external/tls")
-                .laddr(([0, 0, 0, 0], 8883).into())
-                .tls_key(Some("/etc/ssl/private/server.key"))
-                .tls_cert(Some("/etc/ssl/certs/server.crt"))
-                .tls_cross_certificate(false)
-                .bind()?
-                .tls()?,
-        )
+        .listener(Builder::new().name("external/tcp").laddr(([0, 0, 0, 0], 8883).into()).bind()?.tcp()?)
         .build()
         .run()
         .await?;
-
     Ok(())
 }
