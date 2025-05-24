c58b619a6694ffd2ceff64a08c7535b311ee1bbd
Giovani-iDE <christian.sup08@gmail.com>
2025-05-24 12:08:41 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use rmqtt::{context::ServerContext, net::Builder, server::MqttServer, Result};
+use simple_logger::SimpleLogger;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .init()?;
+
+    let scx = ServerContext::new().plugins_dir("plugins/").build().await;
+
+
+    MqttServer::new(scx)
+        .listener(
+            Builder::new()
+                .name("external/tls")
+                .laddr(([0, 0, 0, 0], 8883).into())
+                .tls_key(Some("./bin/server.key"))
+                .tls_cert(Some("./bin/server.crt"))
+                .tls_cross_certificate(false)
+                .bind()?
+                .tls()?,
+        )
+        .build()
+        .run()
+        .await?;
+
+    Ok(())
+}
