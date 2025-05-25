f0e0e45ec1af068b47d61f976d4caf7b070296d6
Giovani-iDE <christian.sup08@gmail.com>
2025-05-24 19:04:19 -0500

    change new serveo change2

--- start.sh
+++ start.sh
@@ -9,4 +9,5 @@ sleep 5
 
 # Abrir túnel TCP en Serveo hacia el puerto 8883 local
 echo "Iniciando túnel Serveo en el puerto 8883..."
-ssh -o StrictHostKeyChecking=no -R 0.0.0.0:8883:localhost:8883 serveo.net
+ssh -o StrictHostKeyChecking=no -R 0:localhost:8883 serveo.net
+
